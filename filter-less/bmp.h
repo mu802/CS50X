e66618b121adb934b988cc0172c90b4db81451e8
mu802 <122411122+mu802@users.noreply.github.com>
2023-03-31 03:10:28 +0000

    Fri, Mar 31, 2023, 6:10 AM +03:00

--- filter-less/bmp.h
+++ filter-less/bmp.h
@@ -2,19 +2,28 @@
 
 #include <stdint.h>
 
-// These data types are essentially aliases for C/C++ primitive data types. 
-// Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
-// See https://en.wikipedia.org/wiki/C_data_types#stdint.h for more on stdint.h.
-
+/**
+ * Common Data Types
+ *
+ * The data types in this section are essentially aliases for C/C++
+ * primitive data types.
+ *
+ * Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
+ * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
+ */
 typedef uint8_t  BYTE;
 typedef uint32_t DWORD;
 typedef int32_t  LONG;
 typedef uint16_t WORD;
 
-// The BITMAPFILEHEADER structure contains information about the type, size,
-// and layout of a file that contains a DIB [device-independent bitmap].
-// Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
-
+/**
+ * BITMAPFILEHEADER
+ *
+ * The BITMAPFILEHEADER structure contains information about the type, size,
+ * and layout of a file that contains a DIB [device-independent bitmap].
+ *
+ * Adapted from http://msdn.microsoft.com/en-us/library/dd183374(VS.85).aspx.
+ */
 typedef struct
 {
     WORD   bfType;
@@ -25,10 +34,14 @@ typedef struct
 } __attribute__((__packed__))
 BITMAPFILEHEADER;
 
-// The BITMAPINFOHEADER structure contains information about the
-// dimensions and color format of a DIB [device-independent bitmap].
-// Adapted from http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
-
+/**
+ * BITMAPINFOHEADER
+ *
+ * The BITMAPINFOHEADER structure contains information about the
+ * dimensions and color format of a DIB [device-independent bitmap].
+ *
+ * Adapted from http://msdn.microsoft.com/en-us/library/dd183376(VS.85).aspx.
+ */
 typedef struct
 {
     DWORD  biSize;
@@ -45,9 +58,14 @@ typedef struct
 } __attribute__((__packed__))
 BITMAPINFOHEADER;
 
-// The RGBTRIPLE structure describes a color consisting of relative intensities of
-// red, green, and blue. Adapted from http://msdn.microsoft.com/en-us/library/aa922590.aspx.
-
+/**
+ * RGBTRIPLE
+ *
+ * This structure describes a color consisting of relative intensities of
+ * red, green, and blue.
+ *
+ * Adapted from http://msdn.microsoft.com/en-us/library/aa922590.aspx.
+ */
 typedef struct
 {
     BYTE  rgbtBlue;
