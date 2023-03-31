c7705fb0b3c7d4dcddc735a68dd420ec3150b5ef
mu802 <122411122+mu802@users.noreply.github.com>
2023-03-31 03:17:19 +0000

    Fri, Mar 31, 2023, 6:17 AM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,92 +1,65 @@
-    // created by : alisharify
-    //Do not copy code
-
-#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef uint8_t byte;
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    //check for 2 arguments
+    // Check usage
     if (argc != 2)
     {
-        printf("Usage Error: ./program-name <filename>\n");
+        printf("Usage: ./recover image\n");
         return 1;
     }
 
-    // open user input file
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
+    // Open file
+    FILE *file = fopen(argv[1], "r");
+    if (!file)
     {
-        printf("Usage Error: Cant open < %s > file\n", argv[1]);
         return 1;
     }
 
-    /* first three byte of jpg picture is --> 0xff 0xd8 0xff
-    AND 0xe0 to 0xef */
-    byte buffer_pic[512];
+    FILE *img = NULL;
 
-    //counter for name of file
+    BYTE buffer[512];
+    char filename[8];
     int counter = 0;
-
-    int jpg_cc = 0;
-    int jpg_one = 1;
-    int flag = 0;
-
-    //global file pointer
-    FILE *pic_G = NULL;
-
-    //read card file
-    while (fread(buffer_pic, sizeof(byte), 512, input))
+    while (fread(buffer, sizeof(BYTE), 512, file) == 512)
     {
-        //check for 3 first byte for jpeg
-        if (buffer_pic[0] == 0xff && buffer_pic[1] == 0xd8 && buffer_pic[2] == 0xff && (buffer_pic[3] & 0xf0) == 0xe0)
+        // checks if start of img in buffer
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-
-            if (jpg_one == 0)
+            // if start of img and first image i.e. counter ==0
+            // then begins writing a new image
+            if (counter == 0)
             {
-                fclose(pic_G);
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                fwrite(&buffer, sizeof(BYTE), 512, img);
+                counter += 1;
             }
-            flag = 1;
-            jpg_cc = 1;
-        }
-        else
-        {
-            jpg_cc = 0;
-        }
-
-        // check for new pic
-        if (jpg_cc == 1)
-        {
-            // in here we definatly find a pic so we update jpg_one
-            //in next round we shoud close curccly file
-            jpg_one = 0;
-
-            //generate file name
-            char filename[255];
-            sprintf(filename, "%03i.jpg", counter);
-            counter++;
-
-            //open out put file
-            pic_G = fopen(filename, "w");
-            if (pic_G == NULL)
+            // if start of img but not first image then
+            // closes the image and begins writing new image
+            else if (counter > 0)
             {
-                return 1;
+                fclose(img);
+                sprintf(filename, "%03i.jpg", counter);
+                img = fopen(filename, "w");
+                fwrite(&buffer, sizeof(BYTE), 512, img);
+                counter += 1;
             }
         }
-
-        // in here with flag we just write to new file
-        if (flag == 1)
+        // if not start of new img
+        // then it keeps on writing the image
+        else if (counter > 0)
         {
-            fwrite(buffer_pic, sizeof(byte), 512, pic);
+            fwrite(&buffer, sizeof(BYTE), 512, img);
         }
 
     }
 
-    //close files
-    fclose(input);
-    fclose(pic_G);
-    return 0;
+    // Close file
+    fclose(file);
+    fclose(img);
+}
