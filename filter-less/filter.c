fccba9bbada0ba1c5d9c87cfb24a9573ad5b664c
mu802 <122411122+mu802@users.noreply.github.com>
2023-03-31 03:08:00 +0000

    Fri, Mar 31, 2023, 6:08 AM +03:00

--- filter-less/filter.c
+++ filter-less/filter.c
@@ -6,7 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-
     // Define allowable filters
     char *filters = "bgrs";
 
@@ -14,21 +13,21 @@ int main(int argc, char *argv[])
     char filter = getopt(argc, argv, filters);
     if (filter == '?')
     {
-        fprintf(stderr, "Invalid filter.\n");
+        printf("Invalid filter.\n");
         return 1;
     }
 
     // Ensure only one filter
     if (getopt(argc, argv, filters) != -1)
     {
-        fprintf(stderr, "Only one filter allowed.\n");
+        printf("Only one filter allowed.\n");
         return 2;
     }
 
     // Ensure proper usage
     if (argc != optind + 2)
     {
-        fprintf(stderr, "Usage: filter [flag] infile outfile\n");
+        printf("Usage: ./filter [flag] infile outfile\n");
         return 3;
     }
 
@@ -40,7 +39,7 @@ int main(int argc, char *argv[])
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
-        fprintf(stderr, "Could not open %s.\n", infile);
+        printf("Could not open %s.\n", infile);
         return 4;
     }
 
@@ -49,7 +48,7 @@ int main(int argc, char *argv[])
     if (outptr == NULL)
     {
         fclose(inptr);
-        fprintf(stderr, "Could not create %s.\n", outfile);
+        printf("Could not create %s.\n", outfile);
         return 5;
     }
 
@@ -67,10 +66,11 @@ int main(int argc, char *argv[])
     {
         fclose(outptr);
         fclose(inptr);
-        fprintf(stderr, "Unsupported file format.\n");
+        printf("Unsupported file format.\n");
         return 6;
     }
 
+    // Get image's dimensions
     int height = abs(bi.biHeight);
     int width = bi.biWidth;
 
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
     RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
     if (image == NULL)
     {
-        fprintf(stderr, "Not enough memory to store image.\n");
+        printf("Not enough memory to store image.\n");
         fclose(outptr);
         fclose(inptr);
         return 7;
@@ -143,11 +143,8 @@ int main(int argc, char *argv[])
     // Free memory for image
     free(image);
 
-    // Close infile
+    // Close files
     fclose(inptr);
-
-    // Close outfile
     fclose(outptr);
-
     return 0;
 }
